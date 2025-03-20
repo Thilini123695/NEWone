430ed31fc1a65a593a446fa84e1d299fa17a2944
Thilini Piyumika <piyumikat80@gmail.com>
2025-03-20 19:57:28 +0530

    Modify conf.php File .

---
+++ conf.php
@@ -0,0 +1,8 @@
+<?php
+// Display a message
+echo "Hello, World!<br>";
+
+// Display the current date and time
+date_default_timezone_set("UTC"); // Set timezone
+echo "Current Date and Time: " . date("Y-m-d H:i:s");
+?>
